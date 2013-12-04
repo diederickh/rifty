f2d3f563d7eccff749d155fef8b6671c9b3fe84c
roxlu <info@roxlu.com>
2013-12-04 15:09:13 +0100

    Added initial files

---
+++ src/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+ 
+#if defined(GLFW_INCLUDE_GLCOREARB)
+#  undef GLFW_INCLUDE_GLCOREARB
+#  define GFLFW_INCLUDE_NONE
+#endif
+ 
+#include <GLXW/glxw.h>
+#include <GLFW/glfw3.h>
+ 
+void key_callback(GLFWwindow* win, int key, int scancode, int action, int mods);
+void error_callback(int err, const char* desc);
+void resize_callback(GLFWwindow* window, int width, int height);
+ 
+int main() {
+ 
+  glfwSetErrorCallback(error_callback);
+ 
+  if(!glfwInit()) {
+    printf("error: cannot setup glfw.\n");
+    return false;
+  }
+ 
+  glfwWindowHint(GLFW_SAMPLES, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  
+  GLFWwindow* win = NULL;
+  int w = 1280;
+  int h = 720;
+ 
+  win = glfwCreateWindow(w, h, "openGL", NULL, NULL);
+  if(!win) {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+ 
+  glfwSetFramebufferSizeCallback(win, resize_callback);
+  glfwSetKeyCallback(win, key_callback);
+  glfwMakeContextCurrent(win);
+  glfwSwapInterval(1);
+ 
+  if(glxwInit() != 0) {
+    printf("error: cannot initialize glxw.\n");
+    ::exit(EXIT_FAILURE);
+  }
+ 
+  while(!glfwWindowShouldClose(win)) {
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+ 
+    glfwSwapBuffers(win);
+    glfwPollEvents();
+  }
+ 
+ 
+  glfwTerminate();
+ 
+  return EXIT_SUCCESS;
+}
+ 
+ 
+void error_callback(int err, const char* desc) {
+  printf("glfw error: %s (%d)\n", desc, err);
+}
+ 
+void key_callback(GLFWwindow* win, int key, int scancode, int action, int mods) {
+  
+  if(action != GLFW_PRESS) {
+    return;
+  }
+ 
+  switch(key) {
+    case GLFW_KEY_LEFT: {
+      break;
+    }
+    case GLFW_KEY_RIGHT: {
+      break;
+    }
+    case GLFW_KEY_ESCAPE: {
+      glfwSetWindowShouldClose(win, GL_TRUE);
+      break;
+    }
+  };
+  
+}
+ 
+void resize_callback(GLFWwindow* window, int width, int height) {
+ 
+}
